9dd4ae34009ef8883d4fb8e14fe8cef0b7fc2112
James Dinan <dinan@mcs.anl.gov>
2010-10-15 16:52:04 -0500

    [svn breadcrumb: r40 @ https://svn.mcs.anl.gov/repos/armci-mpi/trunk]

--- armcix.h
+++ armcix.h
@@ -1,16 +1,6 @@
 #ifndef _ARMCIX_H_
 #define _ARMCIX_H_
 
-typedef struct {
-  ARMCI_Group *groups;  // Array of group handles
-  int ngroups;          // Length of the array
-  int mygroup;          // Index of my group in the array
-} ARMCIX_Group_partition_t;
-
-ARMCIX_Group_partition_t *ARMCIX_Group_partition(int grp_size, ARMCI_Group *parent_grp);
-void         ARMCIX_Group_partition_free(ARMCIX_Group_partition_t *g);
-ARMCI_Group *ARMCIX_Group_partition_mygroup(ARMCIX_Group_partition_t *g);
-
 int ARMCIX_Group_split(ARMCI_Group *parent, int color, int key, ARMCI_Group *new_group);
 
 #endif
