0beb2dd02ba0490ccb2e54bb16fca3963f044f45
James Dinan <dinan@mcs.anl.gov>
2010-12-20 17:10:04 -0600

    Adding the standard ARMCI performance test.
    [svn breadcrumb: r188 @ https://svn.mcs.anl.gov/repos/armci-mpi/trunk]

--- benchmarks/Makefile.mk
+++ benchmarks/Makefile.mk
@@ -2,7 +2,11 @@
 # Copyright (C) 2010. See COPYRIGHT in top-level directory.
 #
 
-check_PROGRAMS += benchmarks/ping-pong benchmarks/ping-pong-mpi benchmarks/ring-flood
+check_PROGRAMS += benchmarks/ping-pong          \
+                  benchmarks/ping-pong-mpi      \
+                  benchmarks/ring-flood         \
+                  benchmarks/armci-perf         \
+                  # end
 
 benchmarks_ping_pong_SOURCES = $(top_srcdir)/benchmarks/ping-pong.c
 benchmarks_ping_pong_LDADD = -larmci
@@ -15,3 +19,7 @@ benchmarks_ping_pong_mpi_DEPENDENCIES = libarmci.la
 benchmarks_ring_flood_SOURCES = $(top_srcdir)/benchmarks/ring-flood.c
 benchmarks_ring_flood_LDADD = -larmci
 benchmarks_ring_flood_DEPENDENCIES = libarmci.la
+
+benchmarks_armci_perf_SOURCES = $(top_srcdir)/benchmarks/armci-perf.c
+benchmarks_armci_perf_LDADD = -larmci -lm
+benchmarks_armci_perf_DEPENDENCIES = libarmci.la
