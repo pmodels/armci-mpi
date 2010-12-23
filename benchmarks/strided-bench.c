7dd4eb32329828f4ffe755590eb0f585d65768bd
James Dinan <dinan@mcs.anl.gov>
2010-12-23 11:30:41 -0600

    First pass at a strided benchmark.
    [svn breadcrumb: r191 @ https://svn.mcs.anl.gov/repos/armci-mpi/trunk]

---
+++ benchmarks/strided-bench.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+
+#include <mpi.h>
+#include <armci.h>
+
+#define MAX_XDIM        1024
+#define MAX_YDIM        1024
+
+#define MAX_DATA_SIZE   (MAX_XDIM*MAX_YDIM*sizeof(double))
+#define NUM_ITERATIONS  10 //((data_size <= 2048) ? 4096 : ((data_size <= 16384) ? 512 : 128))
+#define NUM_WARMUP_ITER 1 
+
+int main(int argc, char ** argv) {
+  int    rank, nproc;
+  int    target_rank, xdim, ydim, test_iter;
+  int    stride[1], count[2], levels, scale;
+  int   *my_data, *buf;
+  void **base_ptrs;
+
+  MPI_Init(&argc, &argv);
+  ARMCI_Init();
+
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+
+  if (rank == 0) printf("Starting one-sided strided performance test with %d processes\n", nproc);
+
+  buf = ARMCI_Malloc_local(MAX_DATA_SIZE);
+  base_ptrs = malloc(sizeof(void*)*nproc);
+  ARMCI_Malloc(base_ptrs, MAX_DATA_SIZE);
+
+  memset(buf, rank, MAX_DATA_SIZE);
+
+  if (rank == 0)
+    printf("%12s %12s %12s %12s %12s %12s %12s %12s\n", "Trg. Rank", "Xdim Ydim",
+        "Get (usec)", "Put (usec)", "Acc (usec)",
+        "Get (MiB/s)", "Put (MiB/s)", "Acc (MiB/s)");
+
+  stride[0] = MAX_XDIM;
+  levels    = 1;
+  scale     = 1.0;
+
+  for (target_rank = 1; rank == 0 && target_rank < nproc; target_rank++) {
+
+    for (xdim = 1; xdim <= MAX_XDIM; xdim *= 2) {
+      count[0] = xdim*sizeof(double);
+
+      for (ydim = 1; ydim <= MAX_YDIM; ydim *= 2) {
+        const int data_size = xdim*ydim*sizeof(double);
+        double    t_get, t_put, t_acc;
+
+        count[1] = ydim;
+
+        for (test_iter = 0; test_iter < NUM_ITERATIONS + NUM_WARMUP_ITER; test_iter++) {
+          if (test_iter == NUM_WARMUP_ITER)
+            t_get = MPI_Wtime();
+
+          ARMCI_GetS(base_ptrs[target_rank], stride, buf, stride, count, levels, target_rank);
+        }
+        t_get = (MPI_Wtime() - t_get)/NUM_ITERATIONS;
+
+        for (test_iter = 0; test_iter < NUM_ITERATIONS + NUM_WARMUP_ITER; test_iter++) {
+          if (test_iter == NUM_WARMUP_ITER)
+            t_put = MPI_Wtime();
+
+          ARMCI_PutS(buf, stride, base_ptrs[target_rank], stride, count, levels, target_rank);
+        }
+        ARMCI_Fence(target_rank);
+        t_put = (MPI_Wtime() - t_put)/NUM_ITERATIONS;
+
+        for (test_iter = 0; test_iter < NUM_ITERATIONS + NUM_WARMUP_ITER; test_iter++) {
+          if (test_iter == NUM_WARMUP_ITER)
+            t_acc = MPI_Wtime();
+
+          ARMCI_AccS(ARMCI_ACC_DBL, (void*) &scale, buf, stride, base_ptrs[target_rank], stride, count, levels, target_rank);
+        }
+        ARMCI_Fence(target_rank);
+        t_acc = (MPI_Wtime() - t_acc)/NUM_ITERATIONS;
+
+        printf("%12d %6d%6d %12.3f %12.3f %12.3f %12.3f %12.3f %12.3f\n", target_rank, xdim, ydim,
+            t_get*1.0e6, t_put*1.0e6, t_acc*1.0e6, data_size/(1024.0*1024.0)/t_get, data_size/(1024.0*1024.0)/t_put, data_size/(1024.0*1024.0)/t_acc);
+      }
+    }
+  }
+
+  ARMCI_Barrier();
+
+  ARMCI_Free(base_ptrs[rank]);
+  ARMCI_Free_local(buf);
+  free(base_ptrs);
+
+  ARMCI_Finalize();
+  MPI_Finalize();
+
+  return 0;
+}
