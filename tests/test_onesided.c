e88b755b0ea19fec78f3464472db6c7b86faa6d6
James Dinan <dinan@mcs.anl.gov>
2011-02-10 14:21:34 -0600

    s/ARMPCI/ARMCI/
    [svn breadcrumb: r241 @ https://svn.mcs.anl.gov/repos/armci-mpi/trunk]

--- tests/test_onesided.c
+++ tests/test_onesided.c
@@ -20,7 +20,7 @@ int main(int argc, char ** argv) {
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &nproc);
 
-  if (rank == 0) printf("Starting ARMPCI test with %d processes\n", nproc);
+  if (rank == 0) printf("Starting ARMCI test with %d processes\n", nproc);
 
   buf = malloc(DATA_SZ);
   base_ptrs = malloc(sizeof(void*)*nproc);
