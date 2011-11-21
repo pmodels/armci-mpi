b70aa74fc1591281bedd8b573a95610467f2ac7a
James Dinan <dinan@mcs.anl.gov>
2011-11-21 15:11:26 -0600

    Make contrib benchmarks optional.
    [svn breadcrumb: r447 @ https://svn.mcs.anl.gov/repos/armci-mpi/trunk]

--- tests/Makefile.mk
+++ tests/Makefile.mk
@@ -129,4 +129,4 @@ tests_test_igop_LDADD = -larmci
 tests_test_igop_DEPENDENCIES = libarmci.la
 
 include tests/ctree/Makefile.mk
-include tests/contrib/Makefile.mk
+-include tests/contrib/Makefile.mk
