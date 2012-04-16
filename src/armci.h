61e7aea2aa60a5b14eb236ba0a8ff6ed54964b98
Jeff Hammond <jhammond@mcs.anl.gov>
2012-04-16 18:03:49 -0500

    defined the ARMCI RMW enum values to match PNNL's armci.h
    [svn breadcrumb: r499 @ https://svn.mcs.anl.gov/repos/armci-mpi/trunk]

--- src/armci.h
+++ src/armci.h
@@ -141,8 +141,11 @@ void  ARMCI_Unlock(int mutex, int proc);
 /** ARMCI Read-Modify-Write API
   */
 
-enum ARMCI_Rmw_e { ARMCI_FETCH_AND_ADD, ARMCI_FETCH_AND_ADD_LONG, 
-                   ARMCI_SWAP, ARMCI_SWAP_LONG };
+/* enum values to match PNNL code */
+enum ARMCI_Rmw_e { ARMCI_SWAP = 10, 
+                   ARMCI_SWAP_LONG = 11,
+                   ARMCI_FETCH_AND_ADD = 12, 
+                   ARMCI_FETCH_AND_ADD_LONG = 13 };
 
 int ARMCI_Rmw(int op, void *ploc, void *prem, int value, int proc);
 
