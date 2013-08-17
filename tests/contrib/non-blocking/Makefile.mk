9a521c04583a48b871459d867798426fa5269623
Pavan Balaji <balaji@mcs.anl.gov>
2013-08-17 14:11:40 -0500

    Fix bad Makefile usage.
    top_srcdir paths were explicitly specified in the makefiles.  This is
    incorrect usage and causes problems with subdir-objects.

--- tests/contrib/non-blocking/Makefile.mk
+++ tests/contrib/non-blocking/Makefile.mk
@@ -6,8 +6,8 @@ check_PROGRAMS += tests/contrib/non-blocking/overlap tests/contrib/non-blocking/
 
 TESTS          += tests/contrib/non-blocking/simple
 
-tests_contrib_non_blocking_overlap_SOURCES = $(top_srcdir)/tests/contrib/non-blocking/overlap.c
+tests_contrib_non_blocking_overlap_SOURCES = tests/contrib/non-blocking/overlap.c
 tests_contrib_non_blocking_overlap_LDADD = libarmci.la -lm
 
-tests_contrib_non_blocking_simple_SOURCES = $(top_srcdir)/tests/contrib/non-blocking/simple.c
+tests_contrib_non_blocking_simple_SOURCES = tests/contrib/non-blocking/simple.c
 tests_contrib_non_blocking_simple_LDADD = libarmci.la -lm
