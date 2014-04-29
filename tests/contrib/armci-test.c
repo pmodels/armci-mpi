0fca8500d80665bf5c805191166e02cad36fd273
Jeff Hammond <jeff.science@gmail.com>
2014-04-29 12:39:17 -0500

    remove manoj, pvm, tcgmsg, bgml, windows

--- tests/contrib/armci-test.c
+++ tests/contrib/armci-test.c
@@ -1,53 +1,14 @@
-/* $Id: test.c,v 1.43.6.6 2007-08-30 22:59:27 manoj Exp $ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <unistd.h>
 
-#ifdef WIN32
-#  include <windows.h>
-#  define sleep(x) Sleep(1000*(x))
-#else
-#  include <unistd.h>
-#endif
-
-/* ARMCI is impartial to message-passing libs - we handle them with MP macros */
-#if defined(PVM)
-#   include <pvm3.h>
-#   ifdef CRAY
-#     define MPGROUP         (char *)NULL
-#     define MP_INIT(arc,argv)
-#   else
-#     define MPGROUP           "mp_working_group"
-#     define MP_INIT(arc,argv) pvm_init(arc, argv)
-#   endif
-#   define MP_FINALIZE()     pvm_exit()
-#   define MP_BARRIER()      pvm_barrier(MPGROUP,-1)
-#   define MP_MYID(pid)      *(pid)   = pvm_getinst(MPGROUP,pvm_mytid())
-#   define MP_PROCS(pproc)   *(pproc) = (int)pvm_gsize(MPGROUP)
-    void pvm_init(int argc, char *argv[]);
-#elif defined(TCGMSG)
-#   include <sndrcv.h>
-    long tcg_tag =30000;
-#   define MP_BARRIER()      SYNCH_(&tcg_tag)
-#   define MP_INIT(arc,argv) PBEGIN_((argc),(argv))
-#   define MP_FINALIZE()     PEND_()
-#   define MP_MYID(pid)      *(pid)   = (int)NODEID_()
-#   define MP_PROCS(pproc)   *(pproc) = (int)NNODES_()
-#elif defined(BGML)
-# include "bgml.h"
-# define MP_BARRIER()      bgml_barrier(3);
-# define MP_FINALIZE()
-# define MP_INIT(arc, argv)
-# define MP_MYID(pid)      *(pid)=BGML_Messager_rank();
-# define MP_PROCS(pproc)   *(pproc)=BGML_Messager_size();
-#else
-#   include <mpi.h>
-#   define MP_BARRIER()      MPI_Barrier(MPI_COMM_WORLD)
-#   define MP_FINALIZE()     MPI_Finalize()
-#   define MP_INIT(arc,argv) MPI_Init(&(argc),&(argv))
-#   define MP_MYID(pid)      MPI_Comm_rank(MPI_COMM_WORLD, (pid))
-#   define MP_PROCS(pproc)   MPI_Comm_size(MPI_COMM_WORLD, (pproc));
-#endif
+#include <mpi.h>
+#define MP_BARRIER()      MPI_Barrier(MPI_COMM_WORLD)
+#define MP_FINALIZE()     MPI_Finalize()
+#define MP_INIT(arc,argv) MPI_Init(&(argc),&(argv))
+#define MP_MYID(pid)      MPI_Comm_rank(MPI_COMM_WORLD, (pid))
+#define MP_PROCS(pproc)   MPI_Comm_size(MPI_COMM_WORLD, (pproc));
 
 #include <armci.h>
 
@@ -89,13 +50,6 @@
 #define MAXPROC 128
 #define TIMES 100
 
-#ifdef CRAY
-# define ELEMS 800
-#else
-# define ELEMS 200
-#endif
-
-
 /***************************** macros ************************/
 #define COPY(src, dst, bytes) memcpy((dst),(src),(bytes))
 #define MAX(a,b) (((a) >= (b)) ? (a) : (b))
@@ -105,43 +59,6 @@
 /***************************** global data *******************/
 int me, nproc;
 void* work[MAXPROC]; /* work array for propagating addresses */
-
-
-
-#ifdef PVM
-void pvm_init(int argc, char *argv[])
-{
-    int mytid, mygid, ctid[MAXPROC];
-    int np, i;
-
-    mytid = pvm_mytid();
-    if((argc != 2) && (argc != 1)) goto usage;
-    if(argc == 1) np = 1;
-    if(argc == 2)
-        if((np = atoi(argv[1])) < 1) goto usage;
-    if(np > MAXPROC) goto usage;
-
-    mygid = pvm_joingroup(MPGROUP);
-
-    if(np > 1)
-        if (mygid == 0) 
-            i = pvm_spawn(argv[0], argv+1, 0, "", np-1, ctid);
-
-    while(pvm_gsize(MPGROUP) < np) sleep(1);
-
-    /* sync */
-    pvm_barrier(MPGROUP, np);
-    
-    printf("PVM initialization done!\n");
-    
-    return;
-
-usage:
-    fprintf(stderr, "usage: %s <nproc>\n", argv[0]);
-    pvm_exit();
-    exit(-1);
-}
-#endif
 
 /*\ generate random range for a section of multidimensional array 
 \*/
@@ -1795,6 +1712,7 @@ int main(int argc, char* argv[])
 /*
        if(me==1)armci_die("process 1 committing suicide",1);
 */
+#if 0
         if(me==0){
            printf("\nTesting strided gets and puts\n");
            printf("(Only std output for process 0 is printed)\n\n"); 
@@ -1872,6 +1790,8 @@ int main(int argc, char* argv[])
         test_swap(); // FIXME
         ARMCI_AllFence();
         MP_BARRIER();
+
+#endif
 
         if(me==0){
            printf("\nTesting register-originated put and get\n");
