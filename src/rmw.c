bbea42d4cd8d32863d31744dac92c8d85040beaa
Jeff Hammond <jeff.science@gmail.com>
2014-07-21 11:05:35 -0700

    use GMR RMW in RMW code with CPP protection

--- src/rmw.c
+++ src/rmw.c
@@ -10,6 +10,7 @@
 #include <gmr.h>
 #include <debug.h>
 
+#if (MPI_VERSION>2)
 /** One-sided fetch-and-op.  Source and output buffer must be private.
   *
   * @param[in] mreg      Memory region
@@ -42,6 +43,7 @@ int gmr_fetch_and_op(gmr_t *mreg, void *src, void *out, void *dst,
 
   return 0;
 }
+#endif
 
 /* -- begin weak symbols block -- */
 #if defined(HAVE_PRAGMA_WEAK)
@@ -69,6 +71,75 @@ int gmr_fetch_and_op(gmr_t *mreg, void *src, void *out, void *dst,
   * @param[in]  value Value to add to remote location (ignored for swap).
   * @param[in]  proc  Process rank for the target buffer.
   */
+#if (MPI_VERSION>2)
+int PARMCI_Rmw(int op, void *ploc, void *prem, int value, int proc) {
+
+  int is_swap = 0, is_long = 0;
+  MPI_Datatype type;
+  MPI_Op       rop;
+  gmr_t *src_mreg, *dst_mreg;
+
+  /* If NOGUARD is set, assume the buffer is not shared */
+  if (ARMCII_GLOBAL_STATE.shr_buf_method != ARMCII_SHR_BUF_NOGUARD)
+    src_mreg = gmr_lookup(ploc, ARMCI_GROUP_WORLD.rank);
+  else
+    src_mreg = NULL;
+
+  dst_mreg = gmr_lookup(prem, proc);
+
+  ARMCII_Assert_msg(dst_mreg != NULL, "Invalid remote pointer");
+
+  if (op == ARMCI_SWAP_LONG || op == ARMCI_FETCH_AND_ADD_LONG) {
+    is_long = 1;
+    type = MPI_LONG;
+  }
+  else
+    type = MPI_INT;
+
+  if (op == ARMCI_SWAP || op == ARMCI_SWAP_LONG) {
+    is_swap = 1;
+    rop = MPI_REPLACE;
+  }
+  else if (op == ARMCI_FETCH_AND_ADD || op == ARMCI_FETCH_AND_ADD_LONG)
+    rop = MPI_SUM;
+  else
+    ARMCII_Error("invalid operation (%d)", op);
+
+  /* We hold the DLA lock if (src_mreg != NULL). */
+
+  if (is_swap) {
+    long out_val_l, src_val_l = *((long*)ploc);
+    int  out_val_i, src_val_i = *((int*)ploc);
+
+    gmr_fetch_and_op(dst_mreg, 
+                     is_long ? (void*) &src_val_l : (void*) &src_val_i /* src */,
+                     is_long ? (void*) &out_val_l : (void*) &out_val_i /* out */,
+    		     prem /* dst */, type, rop, proc);
+    gmr_flush(dst_mreg, proc, 0); /* it's a round trip so w.r.t. flush, local=remote */
+    if (is_long)
+      *(long*) ploc = out_val_l;
+    else
+      *(int*) ploc = out_val_i;
+  }
+  else /* fetch-and-add */ {
+    long fetch_val_l, add_val_l = value;
+    int  fetch_val_i, add_val_i = value;
+
+    gmr_fetch_and_op(dst_mreg,
+                     is_long ? (void*) &add_val_l   : (void*) &add_val_i   /* src */,
+                     is_long ? (void*) &fetch_val_l : (void*) &fetch_val_i /* out */,
+                     prem /* dst */, type, rop, proc);
+    gmr_flush(dst_mreg, proc, 0); /* it's a round trip so w.r.t. flush, local=remote */
+
+    if (is_long)
+      *(long*) ploc = fetch_val_l;
+    else
+      *(int*) ploc = fetch_val_i;
+  }
+
+  return 0;
+}
+#else // MPI_VERSION<=2
 int PARMCI_Rmw(int op, void *ploc, void *prem, int value, int proc) {
   int           is_long;
   gmr_t *mreg;
@@ -126,3 +197,4 @@ int PARMCI_Rmw(int op, void *ploc, void *prem, int value, int proc) {
 
   return 0;
 }
+#endif
