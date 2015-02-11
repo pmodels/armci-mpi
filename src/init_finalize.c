7187df2b0022f2f6d499edb4a1bf2c72bf40e62a
Jeff Hammond <jeff.science@gmail.com>
2015-02-11 10:55:53 -0800

    more warning such that it is O(1) not O(nproc)

--- src/init_finalize.c
+++ src/init_finalize.c
@@ -151,15 +151,6 @@ int PARMCI_Init(void) {
 #endif
   ARMCII_GLOBAL_STATE.use_win_allocate=ARMCII_Getenv_bool("ARMCI_USE_WIN_ALLOCATE", win_alloc_default);
 
-  if (ARMCII_GLOBAL_STATE.use_win_allocate) {
-      /* Jeff: Using win_allocate leads to correctness issues with some
-       *       MPI implementations since 3c4ad2abc8c387fcdec3a7f3f44fa5fd75653ece. */
-      /* This is required on Cray systems with CrayMPI 7.0.0 (at least) */
-      /* Update (Feb. 2015): Xin and Min found the bug in Fetch_and_op and 
-       *                     it is fixed upstream. */
-      ARMCII_Warning("MPI_Win_allocate can lead to correctness issues.\n");
-  }
-
   /* Setup groups and communicators */
 
   MPI_Comm_dup(MPI_COMM_WORLD, &ARMCI_GROUP_WORLD.comm);
@@ -188,6 +179,15 @@ int PARMCI_Init(void) {
 #endif
 
       printf("  WINDOW type used       = %s\n", ARMCII_GLOBAL_STATE.use_win_allocate ? "ALLOCATE" : "CREATE");
+      if (ARMCII_GLOBAL_STATE.use_win_allocate) {
+          /* Jeff: Using win_allocate leads to correctness issues with some
+           *       MPI implementations since 3c4ad2abc8c387fcdec3a7f3f44fa5fd75653ece. */
+          /* This is required on Cray systems with CrayMPI 7.0.0 (at least) */
+          /* Update (Feb. 2015): Xin and Min found the bug in Fetch_and_op and 
+           *                     it is fixed upstream. */
+          ARMCII_Warning("MPI_Win_allocate can lead to correctness issues.\n");
+      }
+
       printf("  STRIDED_METHOD         = %s\n", ARMCII_Strided_methods_str[ARMCII_GLOBAL_STATE.strided_method]);
       printf("  IOV_METHOD             = %s\n", ARMCII_Iov_methods_str[ARMCII_GLOBAL_STATE.iov_method]);
 
