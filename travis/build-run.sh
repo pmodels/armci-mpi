e648f86c6f6e8a0f1b1a7261903534e2bb56e632
Jeff Hammond <jeff.science@gmail.com>
2015-11-02 13:22:28 -0800

    OpenMPI workarounds in Travis

--- travis/build-run.sh
+++ travis/build-run.sh
@@ -3,6 +3,8 @@
 # Exit on error
 set -ev
 
+MPI_IMPL="$1"
+
 # Environment variables
 export CFLAGS="-std=c99"
 #export MPICH_CC=$CC
@@ -13,4 +15,16 @@ export MPICC=mpicc
 ./configure --enable-g --disable-static
 
 # Run unit tests
-make check
+export ARMCI_VERBOSE=1
+case "$MPI_IMPL" in
+    openmpi)
+        # OpenMPI RMA datatype support was (is?) broken...
+        export ARMCI_STRIDED_METHOD=IOV
+        export ARMCI_IOV_METHOD=BATCHED
+        make check
+        ;;
+    *)
+        # MPICH and derivatives support RMA datatypes...
+        make check
+        ;;
+esac
